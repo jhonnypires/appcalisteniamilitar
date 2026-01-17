1442ec3269f61b289217f34bc2b4600ed7c35b3a
Jhonny Pires <jhonnypires@gmail.com>
2026-01-17 19:48:28 -0300

    fix: force dynamic rendering for result and video pages

--- src/app/quiz/result/page.tsx
+++ src/app/quiz/result/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useNavigateWithParams } from "@/hooks/useNavigateWithParams";
 
 export default function QuizResult() {
--- src/app/quiz/video/page.tsx
+++ src/app/quiz/video/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect, memo } from "react";
 
 import { useSearchParams } from "next/navigation";
